612987eb57aa9ac1c99ef110a88764070a396330
Sarah Akbar <sarahakbar16@gmail.com>
2024-04-30 13:58:38 -0400

    added build changes

--- vite.config.ts
+++ vite.config.ts
@@ -8,12 +8,16 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
-  },
-  server: {
-    host: '127.0.0.1',
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      onwarn(warning, warn) {
+        // Suppress "Module level directives cause errors when bundled" warnings
+        if (warning.code === 'MODULE_LEVEL_DIRECTIVE') {
+          return;
+        }
+        warn(warning);
+      },
+    },
   },
 });
